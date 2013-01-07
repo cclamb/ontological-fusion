ace29ad5e8c74c4e1ea06abc589e652c37204b01
cclamb <cclamb@cs.unm.edu>
2013-01-07 06:05:22 -0700

    implemented ruby interpreter, but major problem with branching ontologies

--- lib/ruby_interpreter.rb
+++ lib/ruby_interpreter.rb
@@ -1,5 +1,21 @@
 class RubyInterpreter
-	def initialize ontology_1, ontology_2
 
+	def initialize ontology_1, ontology_2
+    raise ArgumentError if ontology_1 == nil || ontology_2 == nil
+    @factory_1, @factory_2 = ontology_1, ontology_2
 	end
+
+	def unify
+
+
+    h1 = ontology_hierarchy @factory_1.call
+    h2 = ontology_hierarchy @factory_2.call
+
+    h2.intersection(h1).to_a
+	end
+
+  def ontology_hierarchy obj
+    ((obj != nil && obj.superclass.to_s != 'Object') ? ontology_hierarchy(obj.superclass) : Set.new) << obj
+  end
+
 end
--- test/specs/ruby_interpreter_spec.rb
+++ test/specs/ruby_interpreter_spec.rb
@@ -2,17 +2,53 @@ require 'rspec'
 
 require_relative '../../lib/ruby_interpreter'
 
-b_1 = Proc.new do 
+b1 = ->() do 
   class Top ; end
   class Bottom < Top ; end
+  return Bottom
 end
 
-b_2 = Proc.new do
+b2 = ->() do
   class Top ; end
+  return Top
+end
+
+c1 = ->() do
+  class Unclassified ; end
+  class Secret < Unclassified ; end
+  class TopSecret < Secret ; end
+  class Magenta < TopSecret ; end
+  class Vermillion < TopSecret ; end
+end
+
+c2 = ->() do
+
 end
 
 describe RubyInterpreter do 
-	it 'should take two ontologies describe in ruby and create a single, unified ontology' do
-    RubyInterpreter.new b_1, b_2
+
+	context 'simple ontologies' do
+
+		it 'should take two ontologies described in ruby and create a single, unified ontology' do
+	    	unified = RubyInterpreter.new(b1, b2).unify
+	    	unified.size.should eq 1
+	    	unified[0].to_s.should eq 'Top'
+		end
+
+    it 'should take the same ontology and return an identical structure' do
+        unified = RubyInterpreter.new(b1, b1).unify
+        unified.size.should eq 2
+        unified[0].to_s.should eq 'Top'
+        unified[1].to_s.should eq 'Bottom'
+    end
+
 	end
+
+  context 'non-trivial ontologies' do
+
+    it 'should return the intersection of two distinct ontologies'
+    it 'should return the intersection of the same ontology'
+
+  end
+
 end 
